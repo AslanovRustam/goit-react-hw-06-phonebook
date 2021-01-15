01fc1a40ed9cd22dbcfa0e3f6e0caed5b053100f
Aslanov Rustam <aslanovrg@gmail.com>
2021-01-16 00:12:16 +0200

    ставит reduxб создает хранилище

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { myAction } from './redux/actions';
+
+// console.log(store);
+// console.log(myAction);
+// console.log(store.dispatch(myAction()));
+store.dispatch(myAction(25));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/actions.js
@@ -0,0 +1,6 @@
+export const myAction = value => {
+  return {
+    type: 'my action',
+    payload: value,
+  };
+};
