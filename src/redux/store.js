c59dc25ee1703290b45a6d2451e059e00ab4a245
Aslanov Rustam <aslanovrg@gmail.com>
2021-01-19 00:38:12 +0200

    добавляет redux-toolkit, рефакторинг кода с ипользованием configureStore(), createAction()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+// import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import contactReducer from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
 
-const reducer = combineReducers({
-  contacts: contactReducer,
-});
+///=====ШАГ №1 - ванильный Redux
+// const reducer = combineReducers({
+//   contacts: contactReducer,
+// });
+// const store = createStore(reducer, composeWithDevTools());
 
-const store = createStore(reducer, composeWithDevTools());
+const store = configureStore({ reducer: { contacts: contactReducer } });
 
 export default store;
